4b486ad03a64b3d586e1a33ac7acbd5efb01675f
Nguyen Dinh Hoang <Hoang.ND235086@sis.hust.edu.vn>
2024-12-05 19:43:03 +0700

    Update GameException.java

--- src/main/java/com/example/sam_loc_2/GameException.java
+++ src/main/java/com/example/sam_loc_2/GameException.java
@@ -1,4 +1,22 @@
 package com.example.sam_loc_2;
 
-public class GameException {
+public class GameException extends RuntimeException {
+    public enum ErrorType {
+        INVALID_MOVE,
+        CANNOT_BEAT_PREVIOUS_MOVE,
+        NOT_ENOUGH_CARDS,
+        GAME_OVER,
+        PLAYER_OUT_OF_CARDS
+    }
+
+    private final ErrorType errorType;
+
+    public GameException(String message, ErrorType errorType) {
+        super(message);
+        this.errorType = errorType;
+    }
+
+    public ErrorType getErrorType() {
+        return errorType;
+    }
 }
